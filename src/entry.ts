02cfb4d2d60b2c692f7d81708903d14eebba84f4
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-11-20 17:57:09 +0400

    Add Express File Server

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,7 @@
 import './config/dotenv';
 
 import Telegram from './services/telegram';
+import * as Server from './services/server';
 
+Server.launch();
 Telegram.launch();
---
+++ src/services/server.ts
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 4444;
+
+app.use(cors());
+app.use(express.static('public'));
+
+export const launch = () => {
+  app.listen(PORT, () => {
+    console.log(`[SERVER] Express Server is now running on ${PORT}`);
+  });
+};
